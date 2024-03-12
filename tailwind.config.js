f5f0643b23b2d0310d8d095501f6b4befe6e7301
dhimas90 <dhimashernandi@gmail.com>
2024-03-12 15:05:19 +0700

    upload 1st time

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center:true,
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
